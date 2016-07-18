ebd96340574b67aa0908d74b0835a5efe1811902
Necto <necto.ne@gmail.com>
2016-07-18 19:51:36 +0200

    More elaborate LoopInProgress attribute.

--- lib/Core/ExecutionState.cpp
+++ lib/Core/ExecutionState.cpp
@@ -752,3 +752,8 @@ SymbolSet CallInfo::computeSymbolicVariablesSet() const {
   return symbols;
 }
 
+LoopInProcess::LoopInProcess(llvm::Loop *_loop,
+                             const ExecutionState &_headerState)
+  :loop(_loop), unfinishedPaths(1), headerAddressSpace(_headerState.addressSpace)
+{
+}
