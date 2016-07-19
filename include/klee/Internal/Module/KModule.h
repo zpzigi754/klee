4eb91a9b7317f2f431bd82803ace58fc7e4c8628
Necto <necto.ne@gmail.com>
2016-07-19 16:09:08 +0200

    Klee remembers now the analyzed loops, so it does not analyze them again.

--- include/klee/Internal/Module/KModule.h
+++ include/klee/Internal/Module/KModule.h
@@ -52,6 +52,9 @@ namespace klee {
     /// Loop information is automatically calculated on initialization
     typedef llvm::LoopInfoBase<llvm::BasicBlock, llvm::Loop> LInfo;
     LInfo loopInfo;
+    /// Keep track of the loops that were analyzed on the subject of
+    /// the invariants.
+    std::set<const llvm::Loop*> analyzedLoops;
 
     /// Whether instructions in this function should count as
     /// "coverable" for statistics and search heuristics.
