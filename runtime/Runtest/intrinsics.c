1d3c44a7f68e009cc40829ffbb5fd32f16ca6418
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2018-03-05 15:40:34 +0100

    Add trivial vigor-specific intrinsic runtime implementaion

--- runtime/Runtest/intrinsics.c
+++ runtime/Runtest/intrinsics.c
@@ -178,7 +178,72 @@ void klee_abort() {
   abort();
 }
 
+int klee_int(const char *name) {
+  int x;
+  klee_make_symbolic(&x, sizeof x, name);
+  return x;
+}
+
 /* not sure we should even define.  is for debugging. */
 void klee_print_expr(const char *msg, ...) { }
 
 void klee_set_forking(unsigned enable) { }
+
+//Vigor: just ignore them
+
+#define KLEE_TRACE_PARAM_PROTO(suffix, type) \
+  void klee_trace_param##suffix(type param, const char* name) {}
+  KLEE_TRACE_PARAM_PROTO(f, float);
+  KLEE_TRACE_PARAM_PROTO(d, double);
+  KLEE_TRACE_PARAM_PROTO(l, long);
+  KLEE_TRACE_PARAM_PROTO(ll, long long);
+  KLEE_TRACE_PARAM_PROTO(_u16, uint16_t);
+  KLEE_TRACE_PARAM_PROTO(_i32, int32_t);
+  KLEE_TRACE_PARAM_PROTO(_i64, int64_t);
+#undef KLEE_TRACE_PARAM_PROTO
+void klee_trace_param_ptr(void* ptr, int width, const char* name) {}
+  void klee_trace_param_ptr_directed(void* ptr, int width,
+                                     const char* name,
+                                     TracingDirection td){}
+  void klee_trace_param_tagged_ptr(void* ptr, int width,
+                                   const char* name, const char* type,
+                                   TracingDirection td){}
+  void klee_trace_param_just_ptr(void* ptr, int width, const char* name){}
+  void klee_trace_param_fptr(void* ptr, const char* name){}
+  void klee_trace_ret(){}
+  void klee_trace_ret_ptr(int width){}
+  void klee_trace_ret_just_ptr(int width){}
+
+  void klee_trace_param_ptr_field(void* ptr, int offset, int width, char* name){}
+  void klee_trace_param_ptr_field_directed(void* ptr, int offset,
+                                           int width, char* name,
+                                           TracingDirection td){}
+  void klee_trace_param_ptr_field_just_ptr(void* ptr, int offset,
+                                           int width, char* name){}
+  void klee_trace_ret_ptr_field(int offset, int width, char* name){}
+  void klee_trace_ret_ptr_field_just_ptr(int offset, int width, char* name){}
+  void klee_trace_param_ptr_nested_field(void* ptr, int base_offset,
+                                         int offset, int width, char* name){}
+  void klee_trace_param_ptr_nested_field_directed(void* ptr, int base_offset,
+                                                  int offset, int width, char* name,
+                                                  TracingDirection td){}
+  void klee_trace_ret_ptr_nested_field(int base_offset,
+                                       int offset, int width, char* name){}
+  void klee_trace_extra_ptr(void* ptr, int width, char* name, char* type, TracingDirection td){}
+  void klee_trace_extra_ptr_field(void* ptr, int offset, int width, char* name, TracingDirection td){}
+  void klee_trace_extra_ptr_field_just_ptr(void* ptr, int offset,
+                                           int width, char* name){}
+  void klee_trace_extra_ptr_nested_field(void* ptr, int base_offset,
+                                         int offset, int width, char* name, TracingDirection td){}
+  void klee_trace_extra_ptr_nested_nested_field(void* ptr, int base_base_offset,
+                                                int base_offset, int offset,
+                                                int width, char* name, TracingDirection td){}
+
+  void klee_forget_all(){}
+
+int klee_induce_invariants(){ return 1; }
+
+  void klee_forbid_access(void* ptr, int width, char* message){}
+  void klee_allow_access(void* ptr, int width){}
+
+  void klee_dump_constraints(){}
