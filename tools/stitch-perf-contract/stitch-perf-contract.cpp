eccbe55769f25b28b7a811f9ff721b1c928f34d7
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-03-28 22:37:28 +0200

    Parse user variables.

--- tools/stitch-perf-contract/stitch-perf-contract.cpp
+++ tools/stitch-perf-contract/stitch-perf-contract.cpp
@@ -21,6 +21,10 @@ llvm::cl::opt<std::string>
                                "describes the data structure performance."),
                 llvm::cl::Required);
 
+llvm::cl::opt<std::string> UserVariables(
+    "user-vars",
+    llvm::cl::desc("Sets the value of user variables (var1=val1,var2=val2)."));
+
 llvm::cl::opt<std::string> InputCallPathFile(llvm::cl::desc("<call path>"),
                                              llvm::cl::Positional,
                                              llvm::cl::Required);
@@ -37,7 +41,7 @@ int main(int argc, char **argv, char **envp) {
   const char *err = NULL;
   void *contract = dlopen(ContractLib.c_str(), RTLD_NOW);
   if ((err = dlerror())) {
-    std::cout << "Error loading contract plugin " << ContractLib << ": " << err
+    std::cerr << "Error loading contract plugin " << ContractLib << ": " << err
               << std::endl;
     exit(-1);
   }
@@ -53,6 +57,34 @@ int main(int argc, char **argv, char **envp) {
   LOAD_SYMBOL(contract, contract_get_sub_contract_performance);
 
   contract_init();
+
+  std::map<std::string, std::string> user_variables =
+      contract_get_user_variables();
+
+  std::string user_variables_string = UserVariables;
+  while (!user_variables_string.empty()) {
+    std::string user_variable_string =
+        user_variables_string.substr(0, user_variables_string.find(","));
+    user_variables_string =
+        user_variable_string.size() == user_variables_string.size()
+            ? ""
+            : user_variables_string.substr(user_variable_string.size() + 1);
+
+    std::string user_var =
+        user_variable_string.substr(0, user_variable_string.find("="));
+    std::string user_val =
+        user_variable_string.substr(user_variable_string.find("=") + 1);
+
+    if (!user_variables.count(user_var)) {
+      std::cerr << "User variable " << user_var << " not defined in contract."
+                << std::endl
+                << "Valid user variables:" << std::endl;
+      for (auto it : user_variables) {
+        std::cerr << "  " << it.first << std::endl;
+      }
+      exit(-1);
+    }
+  }
 
   return 0;
 }
