f759a3370f0731fde4eb194403ceb6cb795734f3
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-04-12 20:03:30 +0200

    Added script to generate all instruction traces.

---
+++ scripts/trace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for KTEST in traces/*.ktest; do
+  DEHAVOCED_KTEST="$KTEST.dehavoced"
+  TRACE="${KTEST%.*}.instructions"
+  echo "Processing $KTEST -> $TRACE"
+
+  $SCRIPT_DIR/../build/bin/ktest-dehavoc $KTEST $DEHAVOCED_KTEST
+
+  export LD_BIND_NOW=1
+  export KTEST_FILE=$DEHAVOCED_KTEST
+
+  pin -t $SCRIPT_DIR/../trace-instructions/pin-trace.so -- \
+      ./executable --wan 1 --lan-dev 0 \
+                   --expire 10 --starting-port 0 --max-flows 65536 || true
+  mv trace.out $TRACE
+done
