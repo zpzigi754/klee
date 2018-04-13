26bc34e4b8cc2ab7ccf3e000d8fac74ee0363839
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-04-13 18:50:23 +0200

    Add together stateful and stateless costs.

--- scripts/stitch-traces.sh
+++ scripts/stitch-traces.sh
@@ -2,19 +2,37 @@
 
 set -euo pipefail
 
-USER_VARS="$1"
+USER_VARS="${1:-}"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ "$USER_VARS" ]; then
-  parallel "$SCRIPT_DIR/../build/bin/stitch-perf-contract \
+  parallel "echo -n \$(basename {} .call_path)' '; \
+            $SCRIPT_DIR/../build/bin/stitch-perf-contract \
                 -use-forked-solver=false \
                 -contract $SCRIPT_DIR/../../vigor/perf-contracts/perf-contracts.so \
                 --user-vars \"$USER_VARS\" \
-                {} 2>/dev/null" ::: traces/*.call_path | sort -n | tail -n 1
+                {} 2>/dev/null" ::: traces/*.call_path > traces/stateful-perf.txt
 else
-  parallel "$SCRIPT_DIR/../build/bin/stitch-perf-contract \
+  parallel "echo -n \$(basename {} .call_path)' '; \
+            $SCRIPT_DIR/../build/bin/stitch-perf-contract \
                 -use-forked-solver=false \
                 -contract $SCRIPT_DIR/../../vigor/perf-contracts/perf-contracts.so \
-                {} 2>/dev/null" ::: traces/*.call_path | sort -n | tail -n 1
+                {} 2>/dev/null" ::: traces/*.call_path > traces/stateful-perf.txt
 fi
+
+awk '
+  {
+    if ($2 < 0 || totals[$1] < 0) {
+      totals[$1] = -1;
+    } else {
+      totals[$1] += $2;
+    }
+  }
+
+  END {
+    for (trace in totals) {
+      print totals[trace];
+    }
+  }' traces/stateful-perf.txt traces/stateless-perf.txt \
+  | sort -n | tail -n 1
