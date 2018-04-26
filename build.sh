6dc4990899da39b9c410cb80056dd2cae9332d93
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-04-26 16:07:19 +0000

    Updated build config.

--- build.sh
+++ build.sh
@@ -8,6 +8,22 @@ KLEE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$KLEE_DIR/build"
 
-[ -f "Makefile" ] || cmake .. -DGTEST_SRC_DIR=/usr/local/src/libgtest -DENABLE_KLEE_UCLIBC=true -DKLEE_UCLIBC_PATH=/usr/local/src/klee-uclibc -DENABLE_POSIX_RUNTIME=true -DENABLE_UNIT_TESTS=false
+[ -f "Makefile" ] || CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" \
+                     CMAKE_PREFIX_PATH="$KLEE_DIR/../z3/build" \
+                     CMAKE_INCLUDE_PATH="$KLEE_DIR/../z3/build/include/" \
+                     cmake \
+                         -DENABLE_UNIT_TESTS=OFF \
+                         -DBUILD_SHARED_LIBS=OFF \
+                         -DENABLE_KLEE_ASSERTS=OFF \
+                         -DLLVM_CONFIG_BINARY="$KLEE_DIR/../llvm/Release/bin/llvm-config" \
+                         -DLLVMCC="$KLEE_DIR/../llvm/Release/bin/clang" \
+                         -DLLVMCXX="$KLEE_DIR/../llvm/Release/bin/clang++" \
+                         -DENABLE_SOLVER_Z3=ON \
+                         -DENABLE_KLEE_UCLIBC=ON \
+                         -DKLEE_UCLIBC_PATH="$KLEE_DIR/../klee-uclibc" \
+                         -DENABLE_POSIX_RUNTIME=ON \
+                         -DCMAKE_BUILD_TYPE=Debug \
+                         ..
+
 
 make -kj $(nproc)
