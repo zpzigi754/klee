c827d1b5ef77b63af726992241e209c29b4c6d48
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-04-30 10:36:00 +0000

    Added status messages.

--- scripts/process-traces.sh
+++ scripts/process-traces.sh
@@ -7,6 +7,11 @@ shift || true
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Generating instruction traces."
 $SCRIPT_DIR/trace-instruction.sh $TRACES_DIR
+
+echo "Computing stateless bounds."
 $SCRIPT_DIR/stateless_perf.sh $TRACES_DIR
+
+echo "Computing stateful bounds."
 $SCRIPT_DIR/stitch-traces.sh $TRACES_DIR $@
