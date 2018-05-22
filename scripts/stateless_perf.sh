6770ee0ea63778795b4160c8e63181c235d936fe
rishabh246 <rishabh246@gmail.com>
2018-05-22 16:28:22 +0200

    Updated main stateless script to output in zero nf

--- scripts/stateless_perf.sh
+++ scripts/stateless_perf.sh
@@ -4,9 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 py_scripts_dir=$SCRIPT_DIR/trace_scripts
 traces_dr=${1:-klee-last}
-insns_output=${2:-stateless-insn-count.txt}
-mem_output=${3:-stateless-mem-insns.txt}
-cycles_output=${4:-stateless-exec-cyles.txt}
+output=${2:-stateless-exec-cyles.txt}
 
 cd $traces_dr
 python $py_scripts_dir/process_trace.py ./
@@ -14,7 +12,7 @@ python $py_scripts_dir/demarcate_trace.py ./ $py_scripts_dir/fn_lists/stateful_f
 python $py_scripts_dir/print_addresses.py ./
 python $py_scripts_dir/formal_cache.py ./
 python $py_scripts_dir/stateless_stats.py ./ comp_insns num_accesses num_hits num_misses trace_nos
-python $py_scripts_dir/stateless_perf.py  comp_insns num_accesses num_hits trace_nos $insns_output $mem_output $cycles_output
+python $py_scripts_dir/stateless_perf.py  comp_insns num_accesses num_hits trace_nos $output 
 
 rm -f $traces_dr/*.packet.demarcated \
       $traces_dr/*.packet_relevant_instructions \
