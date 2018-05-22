6329b471953b26c405321dec2e55e772357abd73
rishabh246 <rishabh246@gmail.com>
2018-05-22 16:26:40 +0200

    Changed output format

--- scripts/trace_scripts/stateless_perf.py
+++ scripts/trace_scripts/stateless_perf.py
@@ -7,16 +7,14 @@ num_insns = sys.argv[1]
 num_accesses_file=sys.argv[2]
 hits_file=sys.argv[3]
 trace_nos = sys.argv[4]
-insns_out = sys.argv[5]
-mem_out = sys.argv[6]
-cycles_out = sys.argv[7]
+perf_out = sys.argv[5]
 
 
 dram_latency = 200
 l1_latency = 2
 cpi = 1 
 
-with open (insns_out,'w') as insns_output, open(mem_out,'w') as mem_output, open(cycles_out,'w') as cycles_output:
+with open (perf_out,'w') as output:
  with open(num_insns,'r') as f1, open(num_accesses_file,'r') as f2,open(hits_file,'r') as f3,open(trace_nos,'r') as f4:
   for line1, line2, line3, line4 in zip(f1,f2,f3,f4):
    l1=int(line1)
@@ -25,9 +23,9 @@ with open (insns_out,'w') as insns_output, open(mem_out,'w') as mem_output, open
    line4 = line4.strip()
    line4 = line4.replace(".packet.stateless_mem_trace.classified","")
    perf = l1*cpi + (l2-l3)*dram_latency + l3*l1_latency
-   insns_output.write(line4 + " " + str(l1+l2)+"\n")
-   mem_output.write(line4 + " " + str(l2)+"\n")
-   cycles_output.write(line4 + " " + str(perf)+"\n")
+   output.write(line4 + " " + "instruction count"+ " " +str(l1+l2)+"\n")
+   output.write(line4 + " " + "memory instructions"+ " "+ str(l2)+"\n")
+   output.write(line4 + " " + "execution cycles"+ " " +str(perf)+"\n")
 	
 
 
