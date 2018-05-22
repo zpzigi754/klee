9f7e2a7355e8aed3213733f835fea8719722a59d
rishabh246 <rishabh246@gmail.com>
2018-05-22 09:51:15 +0200

    Stateless code also returns multiple metrics

--- scripts/trace_scripts/stateless_perf.py
+++ scripts/trace_scripts/stateless_perf.py
@@ -7,14 +7,16 @@ num_insns = sys.argv[1]
 num_accesses_file=sys.argv[2]
 hits_file=sys.argv[3]
 trace_nos = sys.argv[4]
-perf_out = sys.argv[5]
+insns_out = sys.argv[5]
+mem_out = sys.argv[6]
+cycles_out = sys.argv[7]
 
 
 dram_latency = 200
 l1_latency = 2
 cpi = 1 
 
-with open (perf_out,'w') as output:
+with open (insns_out,'w') as insns_output, open(mem_out,'w') as mem_output, open(cycles_out,'w') as cycles_output:
  with open(num_insns,'r') as f1, open(num_accesses_file,'r') as f2,open(hits_file,'r') as f3,open(trace_nos,'r') as f4:
   for line1, line2, line3, line4 in zip(f1,f2,f3,f4):
    l1=int(line1)
@@ -23,7 +25,9 @@ with open (perf_out,'w') as output:
    line4 = line4.strip()
    line4 = line4.replace(".packet.stateless_mem_trace.classified","")
    perf = l1*cpi + (l2-l3)*dram_latency + l3*l1_latency
-   output.write(line4 + " " + str(perf)+"\n")
-
+   insns_output.write(line4 + " " + str(l1+l2)+"\n")
+   mem_output.write(line4 + " " + str(l2)+"\n")
+   cycles_output.write(line4 + " " + str(perf)+"\n")
+	
 
 
