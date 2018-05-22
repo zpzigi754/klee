e90f10cef11e3327a4f0ebc617ca7c264fd65060
rishabh246 <rishabh246@gmail.com>
2018-05-22 09:51:15 +0200

    Stateless code also returns multiple metrics

--- scripts/stateless_perf.sh
+++ scripts/stateless_perf.sh
@@ -4,15 +4,17 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 py_scripts_dir=$SCRIPT_DIR/trace_scripts
 traces_dr=${1:-klee-last}
-output=${2:-stateless-perf.txt}
+insns_output=${2:-stateless-insn-count.txt}
+mem_output=${2:-stateless-mem-insns.txt}
+cycles_output=${2:-stateless-exec-cyles.txt}
 
 cd $traces_dr
 python $py_scripts_dir/process_trace.py ./
 python $py_scripts_dir/demarcate_trace.py ./ $py_scripts_dir/fn_lists/stateful_fns.txt $py_scripts_dir/fn_lists/dpdk_fns.txt $py_scripts_dir/fn_lists/time_fns.txt $py_scripts_dir/fn_lists/verif_fns.txt 
 python $py_scripts_dir/print_addresses.py ./
 python $py_scripts_dir/formal_cache.py ./
-python $py_scripts_dir/stateless_stats.py ./ num_insns num_accesses num_hits num_misses trace_nos
-python $py_scripts_dir/stateless_perf.py  num_insns num_accesses num_hits trace_nos $output
+python $py_scripts_dir/stateless_stats.py ./ comp_insns num_accesses num_hits num_misses trace_nos
+python $py_scripts_dir/stateless_perf.py  comp_insns num_accesses num_hits trace_nos $insns_output $mem_output $cycles_output
 
 rm -f $traces_dr/*.packet.demarcated \
       $traces_dr/*.packet_relevant_instructions \
