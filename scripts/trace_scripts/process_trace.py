93444efaa4ce60df3ec81c499758298f77282267
Rishabh Iyer <rishabh@worf.maas>
2018-05-24 20:25:45 +0000

    Updated script for verify hardware traces

--- scripts/trace_scripts/process_trace.py
+++ scripts/trace_scripts/process_trace.py
@@ -18,23 +18,39 @@ def main():
      trace_lines = list(line for line in trace_lines if line)
      dump_file=file.replace('.instructions','.packet_relevant_instructions')
      packet_code = 0
+     start = 0
+     end = -1
      with open(dump_file,"w") as output:
-      for text in trace_lines:
-       index1 = find_nth(text,"|",2)
-       index2 = find_nth(text,"|",3)
-       fn = text[index1+1:index2].strip()
-       if(packet_code == 0):
-        if(fn == "rte_eth_rx_burst"):
-         packet_code = 1
+       for text in trace_lines:
+        index1 = find_nth(text,"|",1)
+        index2 = find_nth(text,"|",2)
+        fn_call_stack = text[index1+1:index2-1]
+        words = fn_call_stack.split()
+        temp_start = 0
+        temp_end = 0
+        for fn in words:
+ 	 if(fn == "rte_eth_rx_burst"):
+          temp_start = 1
+          continue
        
-       if(packet_code == 1):
-        if(fn == "exit@plt"):
-         packet_code = 2
-  
-       if(packet_code == 1):
-        output.write(text)
-        output.write("\n")
+         if(fn == "rte_eth_tx_burst"):
+          temp_end = 1
+	  continue
         
+        if(start == 0 and temp_start == 1):
+	 start = 1
+	
+	if(end == -1 and temp_end == 1):
+	 end = 0
+	
+	if(end == 0 and temp_end == 0):
+	 end = 1
+
+	if(start>end and start >0):
+         output.write(text)
+         output.write("\n")
+        elif (start<=end):
+	 break
 
 
 
