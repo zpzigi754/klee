c2369964ce0b1d0c736bf97d6de45fde41cc8c0e
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2018-05-30 11:23:15 +0000

    fix the process_trace bash syntax

--- scripts/trace_scripts/process_trace.sh
+++ scripts/trace_scripts/process_trace.sh
@@ -7,25 +7,23 @@ if [ "$inp" -nt "$op" ]; then
 
   if grep -q "rte_eth_rx_burst" $1; then  
     START=$(grep -n -m 1 "rte_eth_rx_burst" $1 |sed  's/\([0-9]*\).*/\1/')
-  else if grep -q "ixgbe_recv" $1; then
+  elif grep -q "ixgbe_recv" $1; then
     START=$(grep -n -m 1 "ixgbe_recv" $1 |sed  's/\([0-9]*\).*/\1/')
   else
     echo "no packet received"
+    echo " " > $2
     exit
   fi
 
 
   if grep -q "rte_eth_tx_burst"  $1; then 
     END=$(grep -n "rte_eth_tx_burst" $1 | tail -1 |sed  's/\([0-9]*\).*/\1/')
-  else if grep -q "ixgbe_xmit_pkts" $1; then
+  elif grep -q "ixgbe_xmit_pkts" $1; then
     END=$(grep -n "ixgbe_xmit_pkts" $1 | tail -1 |sed  's/\([0-9]*\).*/\1/')
   else
     END=$(grep -n "exit@plt" $1 | tail -1 |sed  's/\([0-9]*\).*/\1/')
   fi
   ENDEND=$((END+1))
   sed -n ""$START","$END"p;"$ENDEND"q" $1 > $2
-
-  else
-    echo " ">$2
-  fi
 fi
+
